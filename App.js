cfb57044ba96e43cd5999e78f7e9da7afa6899b2
PeterChauYEG <peter.chau.esq@gmail.com>
2018-09-23 10:43:16 -0600

    integrated remote dev tools

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ import React, { Component } from 'react'
 
 // redux
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 // reducer
 import rootReducer from './rootReducer'
@@ -22,8 +23,16 @@ type Props = {}
 export default class App extends Component<Props> {
   render () {
     
+    // create middleware 
+    const middleware = []
+    
+    // create enhancers
+    const enhancers = composeWithDevTools(
+      applyMiddleware(...middleware)
+    )
+    
     // create the redux store
-    const store = createStore(rootReducer)
+    const store = createStore(rootReducer, enhancers)
 
     return (
       <Provider store={store}>
