c9b164742add6585999d3fcc13e4fac937bc7a90
PeterChauYEG <peter.chau.esq@gmail.com>
2018-09-27 08:14:39 -0600

    button press test working for erroroccurred

--- Components/__tests__/ErrorOccurred.js
+++ Components/__tests__/ErrorOccurred.js
@@ -1,7 +1,11 @@
 import React from 'react'
-// import { shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 import ErrorOccurred from '../ErrorOccurred'
 import renderer from 'react-test-renderer'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('ErrorOccurred', () => {
   it('renders correctly', () => {
@@ -11,16 +15,14 @@ describe('ErrorOccurred', () => {
     expect(tree).toMatchSnapshot()
   })
 
-  // it('simulates "Start over" button click', () => {
-  //   const clearLyrics = jest.fn()
-  //
-  //   const wrapper = shallow(
-  //     <ErrorOccurred clearLyrics={clearLyrics}
-  //     />
-  //   )
-  //
-  //   wrapper.find('Button').simulate('click')
-  //
-  //   expect(clearLyrics).toHaveBeenCalled()
-  // })
+  it('simulates "Start over" button click', () => {
+    const clearLyrics = jest.fn()
+
+    const wrapper = shallow(
+      <ErrorOccurred clearLyrics={clearLyrics}
+      />
+    )
+    wrapper.find('Button').simulate('press') 
+    expect(clearLyrics).toHaveBeenCalled()
+  })
 })
