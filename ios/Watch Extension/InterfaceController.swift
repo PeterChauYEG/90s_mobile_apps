3fa3d639be91b4944d71b2b8999723f2e4351074
PeterChauYEG <peter.chau.esq@gmail.com>
2018-10-06 13:28:28 -0600

    set up watch app

---
+++ ios/Watch Extension/InterfaceController.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceController.swift
+//  Watch Extension
+//
+//  Created by Laptop on 2018-10-06.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+  @IBOutlet weak var predictionLabel: WKInterfaceLabel!
+  
+}
